b78ec8d57daa3a4827ef1159ea8e44e6051872d5
Matan <matan203040@gmail.com>
2025-05-17 21:52:46 +0300

    Add tests across the system

---
+++ ResourceStorageChain.Tests/StorageImplementations/FileSystemStorageTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ResourceStorageChain.StorageImplementations;
+using Xunit;
+
+namespace ResourceStorageChain.Tests.StorageImplementations
+{
+    public class FileSystemStorageTests
+    {
+        [Fact]
+        public async Task SetAndGet_ShouldReturnSameValue_IfNotExpired()
+        {
+            string filePath = Path.GetTempFileName();
+            var storage = new FileSystemStorage<string>(filePath, TimeSpan.FromMinutes(5));
+            string expected = "Hello";
+
+            await storage.Set(expected);
+            var actual = await storage.Get();
+
+            Assert.Equal(expected, actual);
+
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnNull_IfFileDoesNotExist()
+        {
+            string filePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid() + ".json");
+            var storage = new FileSystemStorage<string>(filePath, TimeSpan.FromMinutes(5));
+
+            var result = await storage.Get();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnNull_IfDataExpired()
+        {
+            string filePath = Path.GetTempFileName();
+            var wrapper = new
+            {
+                Value = "Outdated",
+                Timestamp = DateTime.Now - TimeSpan.FromMinutes(10)
+            };
+            await File.WriteAllTextAsync(filePath, JsonSerializer.Serialize(wrapper));
+
+            var storage = new FileSystemStorage<string>(filePath, TimeSpan.FromMinutes(5));
+
+            var result = await storage.Get();
+
+            Assert.Null(result);
+
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public async Task Set_ShouldUpdateLastUpdate()
+        {
+            string filePath = Path.GetTempFileName();
+            var storage = new FileSystemStorage<string>(filePath, TimeSpan.FromMinutes(5));
+
+            await storage.Set("Test");
+            var timestamp = storage.LastUpdate;
+
+            Assert.NotNull(timestamp);
+            Assert.True((DateTime.Now - timestamp.Value).TotalSeconds < 2);
+
+            File.Delete(filePath);
+        }
+    }
+}
---
+++ ResourceStorageChain.Tests/StorageImplementations/WebServiceStorageTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using ResourceStorageChain.StorageImplementations;
+using Xunit;
+
+namespace ResourceStorageChain.Tests.StorageImplementations
+{
+    public class WebServiceStorageTests
+    {
+        private class MockHttpMessageHandler : HttpMessageHandler
+        {
+            private readonly string _responseContent;
+            public MockHttpMessageHandler(string responseContent)
+            {
+                _responseContent = responseContent;
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(_responseContent)
+                };
+                return Task.FromResult(response);
+            }
+        }
+
+        [Fact]
+        public async Task Get_ShouldParseValidResponse()
+        {
+            string mockJson = "\"Hello World\"";
+            var handler = new MockHttpMessageHandler(mockJson);
+            var httpClient = new HttpClient(handler);
+
+            var storage = new WebServiceStorage<string>(
+                "http://fake.url",
+                stream => JsonSerializer.Deserialize<string>(stream)
+            );
+
+            typeof(WebServiceStorage<string>)
+                .GetField("_httpClient", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!
+                .SetValue(storage, httpClient);
+
+            var result = await storage.Get();
+
+            Assert.Equal("Hello World", result);
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnNull_OnException()
+        {
+            var faultyHandler = new MockHttpMessageHandlerThrowing();
+            var httpClient = new HttpClient(faultyHandler);
+            var storage = new WebServiceStorage<string>("http://bad.url", _ => null);
+            typeof(WebServiceStorage<string>)
+                .GetField("_httpClient", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)!
+                .SetValue(storage, httpClient);
+
+            var result = await storage.Get();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Set_ShouldThrowNotSupportedException()
+        {
+            var storage = new WebServiceStorage<string>("http://fake.url", _ => "parsed");
+
+            Assert.ThrowsAsync<NotSupportedException>(() => storage.Set("data"));
+        }
+
+        private class MockHttpMessageHandlerThrowing : HttpMessageHandler
+        {
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                throw new HttpRequestException("Mock failure");
+            }
+        }
+    }
+}
